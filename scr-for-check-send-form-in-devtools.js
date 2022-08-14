69a8f28526085e1be3794178bdea0965a0d286bc
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-08-14 22:45:03 +0300

    addes 2  assistants

---
+++ scr-for-check-send-form-in-devtools.js
@@ -0,0 +1,11 @@
+(() => {
+    document 
+    .querySelector('.js-speaker-form')
+    .addEventListener('subnit', e => {
+      e.preventDefault();
+
+        new FormData(e.currentTarget).forEach((value, name) => 
+        console.log(`${name}: ${value}`),
+        );
+    });
+})();
